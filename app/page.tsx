ebf82ed5549c0179b5429a595bb47ddd03ec551e
Kelvin Akaba <akabakelvin@gmail.com>
2024-08-29 21:27:42 +0000

    login logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Cd from "@/components/login/cd";
 import Login from "@/components/login/login";
 
 export default function Home() {
   return (
     <div>
-      {/* <Cd /> */}
       <Login />
     </div>
   );
--- components/login/login.tsx
+++ components/login/login.tsx
@@ -5,32 +5,60 @@ import image from "@/assets/images/clip-message-sent 1.png";
 import { Button } from '../ui/button';
 import Spinner from '../ui/spinner';
 import { useRouter } from 'next/navigation';
-import { useForm, SubmitHandler } from 'react-hook-form';
 import toast from 'react-hot-toast';
+import axios from 'axios';
 
 interface Input {
     email: string;
     password: string;
+    client: string;
+    // remember: boolean;
 };
 
 const Login = () => {
     const { push } = useRouter();
     const [loading, setLoading] = useState<Boolean>(false);
+    const [form, setForm] = useState<Input>({
+        email: "",
+        password: "",
+        client: "",
+        // remember: false
+    });
 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<Input>()
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const { name, value, type, checked } = event.target;
+        setForm((prevForm) => ({
+            ...prevForm,
+            [name]: type === "checkbox" ? checked : value,
+        }));
+    };
 
-    const onSubmit: SubmitHandler<Input> = async (data) => {
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
         try {
-            console.log(data)
-            toast.success("Success")
-        } catch (error) {
-            console.log(error)
-            toast.error("Didn't work")
+            setLoading(true);
+            const response = await axios.post(
+                'http://165.22.70.167:9100/apicore/v1/users/auth',
+                form
+            );
+            console.log('Data:', response.data);
+            console.log('Status:', response.status);
+            console.log('Text:', response.statusText);
+            toast.success("Login Successful");
+        } catch (error: any) {
+            console.log(error);
+            // console.error('Axios error:', error.response?.data || error.message);
+            toast.error("Login Unsuccessful, Please try again!");
+        } finally {
+            setLoading(false);
+            setForm({
+                email: "",
+                password: "",
+                client: "",
+                // remember: false
+            });
         }
     };
 
@@ -39,46 +67,88 @@ const Login = () => {
 
             <div className="w-full bg-white lg:w-1/2 flex items-center justify-center">
                 <div className="max-w-md w-full p-5">
-                    <h1 className="self-start text-3xl font-semibold mb-3 text-[#3751FE]">
+                    <h1 className="self-start text-[26px] font-bold mb-3 text-[#3751FE]">
                         Artificial Intelligence Driving Results For The Travel Industry
                     </h1>
-                    <h1 className="text-sm font-semibold mb-6 text-gray-500">
+                    <h1 className="text-sm font-semibold mb-6 text-[#000000]">
                         Welcome back! Please login to your account.
                     </h1>
 
-                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                    <form onSubmit={onSubmit} className="space-y-4">
 
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-                            <input
-                                type="text"
-                                required
-                                {...register("email")}
-                                className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
-                            />
+                        <div className="">
+                            <div className="border p-2 focus-within:border-l-[#3751FE] focus-within:border-l-4">
+                                <label htmlFor="email" className="text-sm">
+                                    Email Address
+                                </label>
+                                <div></div>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value={form.email}
+                                    onChange={handleInputChange}
+                                    required
+                                    placeholder="Enter your email"
+                                    className="mt-1 w-full outline-none overflow-ellipsis overflow-hidden placeholder-[#3751FE] autofill:shadow-[inset_0_0_0px_1000px_white] autofill:text-[#3751FE]"
+                                />
+                            </div>
+                            <div className="flex flex-col border p-2 focus-within:border-l-[#3751FE] focus-within:border-l-4">
+                                <label htmlFor="password" className="text-sm">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    value={form.password}
+                                    onChange={handleInputChange}
+                                    placeholder="***************"
+                                    required
+                                    className="mt-1 overflow-hidden outline-none placeholder-[#3751FE] autofill:shadow-[inset_0_0_0px_1000px_white] autofill:text-fill-black"
+                                />
+                            </div>
+                            <div className="flex flex-col border p-2 focus-within:border-l-[#3751FE] focus-within:border-l-4">
+                                <label htmlFor="client" className="text-sm">
+                                    Client
+                                </label>
+                                <input
+                                    type="client"
+                                    name="client"
+                                    id="client"
+                                    value={form.client}
+                                    onChange={handleInputChange}
+                                    placeholder="Enter Client Id"
+                                    className="mt-1 overflow-hidden outline-none placeholder-[#3751FE] autofill:shadow-[inset_0_0_0px_1000px_white] autofill:text-[#3751FE]"
+                                />
+                            </div>
                         </div>
-                        <div>
-                            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                            <input
-                                type="password"
-                                required
-                                {...register("password")}
-                                className="mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300"
-                            />
-                        </div>
+
                         <div className="flex flex-row justify-between w-full">
-                            <label htmlFor="password" className="!text-slate-900">
-                                Password
-                            </label>
-                            <label className="!text-primary cursor-pointer">
+                            <div className="flex items-start mb-5 ml-5">
+                                <div className="flex items-center h-5">
+                                    <input
+                                        name='remember'
+                                        id="remember"
+                                        type="checkbox"
+                                        // checked={form.remember}
+                                        onChange={handleInputChange}
+                                        className="w-3 h-3 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                                    />
+                                </div>
+                                <label htmlFor="remember" className="ms-2 text-sm text-[#000000]">
+                                    Remember me
+                                </label>
+                            </div>
+                            <label className="text-sm text-[#000000] cursor-pointer">
                                 Forgot password?
                             </label>
                         </div>
-                        <div className='flex items-center gap-2'>
+                        <div className='mt-5 flex items-center gap-6'>
                             <Button
                                 type="submit"
-                                className="rounded-none h-10"
-                            // disabled={Boolean(loading) || !isValid}
+                                className="rounded-none h-10 w-28"
+                                disabled={Boolean(loading)}
                             >
                                 {loading ? (
                                     <>
@@ -91,27 +161,19 @@ const Login = () => {
                             </Button>
                             <Button
                                 variant={"outline"}
-                                type="submit"
-                                // disabled={Boolean(loading) || !isValid}
-                                className="rounded-none h-10"
+                                type="button"
+                                className="rounded-none h-10 w-28"
                             >
-                                {loading ? (
-                                    <>
-                                        <Spinner />
-                                        <p>Loading</p>
-                                    </>
-                                ) : (
-                                    "Sign Up"
-                                )}
+                                SIGN UP
                             </Button>
                         </div>
                     </form>
 
-                    <div className="mt-4 text-sm flex items-center justify-between">
-                        <p className='text-gray-600'>or with email</p>
-                        <p className='text-[#3751FE]'>Facebook</p>
-                        <p className='text-[#3751FE]'>LinkedIn</p>
-                        <p className='text-[#3751FE]'>Google</p>
+                    <div className="mt-12 text-sm flex items-center justify-between">
+                        <p className='text-[#000000]'>or with email</p>
+                        <p className='text-[#3751FE] font-bold'>Facebook</p>
+                        <p className='text-[#3751FE] font-bold'>LinkedIn</p>
+                        <p className='text-[#3751FE] font-bold'>Google</p>
                     </div>
                 </div>
             </div>
